38a738d59a917ee2b27de37a832c134223e91b4f
mosesnwaeze <ogbonnasmart@gmail.com>
2023-09-10 09:43:05 +0100

    commiting changes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-//specify the endpoint for each resource here
 
 app.use("/api/v1",route);
 
--- src/models/wallet.js
+++ src/models/wallet.js
@@ -1,30 +1,30 @@
 module.exports = [
    {
-     id: 000001,
+     id: 1,
      userid: 'c231139d-1369-4a3e-975f-c0e0dcc0ab42',
      currentAmount: 100000000,
      userPin:4732
    },
    {
-    id: 000002,
+    id: 2,
     userid: 'ce01c852-7288-49c9-b28d-0357f35efa7d',
     currentAmount: 50000000,
     userPin:3320
    },
    {
-    id: 000003,
+    id: 3,
     userid: '3126bb32-5ed2-41ce-b706-e22ae37cb387',
     currentAmount: 25000,
     userPin:2017
    },
    {
-    id: 000004,
+    id: 4,
     userid: '50d29183-f80d-4e28-9079-740b019a837a',
     currentAmount: 150000,
     userPin:3311
    },
    {
-    id: 000005,
+    id: 5,
     userid: '76dc1312-6a7a-4bb2-9568-415d9eaa17db',
     currentAmount: 1500000,
     userPin:2231
